d4bf93efdcf012589a52fbefc0dcdbaac49bbefe
Mélissa Laplanche <laplanche.melissa@gmail.com>
2025-05-05 17:41:34 +0200

    Ajustements de la structure du projet

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,22 +1,20 @@
-import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/home";
 import About from "./pages/About/about";
-import Accommodation from './pages/Accommodation/accommodation';
-import Error from './pages/Error/error';
+import Lodging from './pages/Lodgings/lodgingPage';
+import NotFound from './pages/NotFound/notFound';
 import Header from "./components/Header/header";
 import Footer from './components/Footer/footer';
 
 function App() {
-  const [activeLodgement, setActive ] = useState(sessionStorage.getItem('activeId')); 
-  return (
+    return (
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/Accommodation/:accommodationId" element={<Accommodation />} />
-        <Route path="*" element={<Error />} />
+        <Route path="/Lodgings/:id" element={<Lodging />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </BrowserRouter>
