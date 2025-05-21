100590e00fb005c8a51c78af86ce29a65706c169
Mélissa Laplanche <laplanche.melissa@gmail.com>
2025-05-21 19:06:21 +0200

    Divers ajustements de scss

--- my-react-app/src/components/Carrousel/Carrousel.jsx
+++ my-react-app/src/components/Carrousel/Carrousel.jsx
@@ -10,10 +10,10 @@ function Carrousel({lodgement}){
     
         const prevImg = () => {
             setActiveIndex(activeIndex === 0 ? length - 1 : activeIndex - 1);  // conditionnel pour ne pas déclencher d'erreurs lorsque les limites de l'array sont atteintes
-            };
+        };
         const nextImg = () => {
             setActiveIndex(activeIndex === length - 1 ? 0 : activeIndex + 1); 
-            };
+        };
         return length>1?(        // Ne pas afficher les fleches ni le nombre d'images si seulement 1
             <div className="carrousel">
                 <i id="previousImage" onClick={prevImg} className="fa fa-solid fa-chevron-left"></i>
